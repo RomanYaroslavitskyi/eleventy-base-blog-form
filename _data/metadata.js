c14a18073b99ba3a0be7a2051f2ca101a61789eb
Roman <Chugaisters@gmail.com>
2024-05-01 16:14:38 +0100

    1

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Roman Yaroslavitskyi`s Blog ",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Roman",
+		email: "roman.yaroslavitskyi@gmail.com",
+		url: "https://example.com/about-me/",
+	},
+};
